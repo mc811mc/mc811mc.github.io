5b9e9dab3d43e6a06969d036d30f461d7c3e06d1
Minho Cho <mc811mc@gmail.com>
2020-09-16 21:28:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-/* temp */
+var gallery = [
+    "Images/Photography/_DSC0105.jpg", 
+    "Images/Photography/_DSC4463.jpg",
+    "Images/Photography/_DSC4727.jpg",
+    "Images/Photography/_DSC5055.jpg",
+    "Images/Photography/_DSC7185.jpg" 
+    ];
+    var num = 0;
+  
+  function next() {
+    var slider = document.getElementById("slider");
+    num++;
+    if(num >= gallery.length) {
+      num = 0;
+    }
+    slider.src = gallery[num];
+    }
+  
+  function prev() {
+    var slider = document.getElementById("slider");
+    num--;
+    if(num < 0) {
+      num = gallery.length-1;
+    }
+    slider.src = gallery[num];
+  }
