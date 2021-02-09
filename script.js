ba3145b9bba74cf0a6af29b41d0cb76bb352c261
Minho Cho <mc811mc@gmail.com>
2021-02-09 17:16:20 +0900

    bulk update

---
+++ script.js
@@ -0,0 +1,18 @@
+//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_website_script
+
+<script>
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousel, 3000);
+}
+</script>
